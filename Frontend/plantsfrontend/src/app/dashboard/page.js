53c1b69547ce68fb915f4f2cdb92fbce27d32693
Gee <gkandandji@gmail.com>
2025-01-28 15:45:04 +0000

    add component template

---
+++ Frontend/plantsfrontend/src/app/dashboard/page.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import AddForm from'@/components/AddForm'
+import PlantCard from '@/components/PlantCard'
+
+const page = () => {
+  return (
+    <div>
+        <AddForm />
+        <PlantCard />
+    </div>
+  )
+}
+
+export default page
